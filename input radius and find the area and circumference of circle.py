56786d191f10fb6cad257524f04e1e8ca70b07c1
Swarnim112006 <143317120+Swarnim112006@users.noreply.github.com>
2023-08-27 20:42:38 +0530

    This is a test website

--- input radius and find the area and circumference of circle.py
+++
@@ -1,8 +0,0 @@
-#Program to input radius and find the area of circle
-import math
-radius=float(input("Enter the radius of circle="))    
-Circumference=2*math.pi*radius
-Area=math.pi*radius**2
-print("Circumference of circle is=",Circumference)
-print("Area of the circle is=",Area)
-
